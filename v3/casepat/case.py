335c99653ed2aec68a435fa340ebc87bb1fb7ab8
Aiumi <aiumi.networks@icloud.com>
2023-08-26 20:20:02 +0200

    correct coments

--- v3/casepat/case.py
+++ v3/casepat/case.py
@@ -11,14 +11,14 @@ https://github.com/eugen-hoppe
 
 class To:
 
-    # Method to convert form camel-case to snake-case
+    # Method to convert from camel-case to snake-case
     # ===============================================
     @staticmethod
     def camel(snake: str) -> str:
         parts = snake.split('_')
         return parts[0] + ''.join(p.capitalize() for p in parts[1:])
 
-    # Method to convert form snake-case to camel-case
+    # Method to convert from snake-case to camel-case
     # ===============================================
     @staticmethod
     def snake(camel: str) -> str:
