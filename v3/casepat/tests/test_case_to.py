21d221bb842b9489341487e34dce96c5779c8f61
Aiumi <aiumi.networks@icloud.com>
2023-08-26 19:12:17 +0200

    test special inputs

--- v3/casepat/tests/test_case_to.py
+++ v3/casepat/tests/test_case_to.py
@@ -5,9 +5,17 @@ TEST_1 = {
     "first_name": "firstName",
     "company_address": "companyAddress"
 }
-
+TEST_2 = {
+    "FirstName": "first_name",
+    "CompanyAddress": "company_address"
+}
 
 def test_camel_snake():
     for snake, camel in TEST_1.items():
         assert case.To.camel(snake) == camel
+        assert case.To.snake(camel) == snake
+
+
+def test_camel_starts_with_upper_char():
+    for camel, snake in TEST_2.items():
         assert case.To.snake(camel) == snake
