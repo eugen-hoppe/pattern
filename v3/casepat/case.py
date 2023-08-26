8bdbae7851aec37276d9293ce33eafd4415b5da2
Aiumi <aiumi.networks@icloud.com>
2023-08-26 20:38:25 +0200

    refactoring

--- v3/casepat/case.py
+++ v3/casepat/case.py
@@ -1,7 +1,7 @@
 """
 Case Pattern 2023
 
-Convert camel-case to snake-case and vise versa
+Converts from snake-case to camel-case and vice versa
 
 Util Models
 
@@ -12,16 +12,16 @@ import re
 
 
 class To:
-
+    
     # Method to convert from camel-case to snake-case
     # ===============================================
     @staticmethod
     def camel(snake: str) -> str:
-        parts = snake.split('_')
-        return parts[0] + ''.join(p.capitalize() for p in parts[1:])
+        parts = snake.split("_")
+        return parts[0] + "".join(p.capitalize() for p in parts[1:])
 
     # Method to convert from snake-case to camel-case
     # ===============================================
     @staticmethod
     def snake(camel: str) -> str:
-        return re.sub(r'([a-z0-9])([A-Z])', r'\1_\2', camel).lower()
+        return re.sub(r"([a-z0-9])([A-Z])", r"\1_\2", camel).lower()
