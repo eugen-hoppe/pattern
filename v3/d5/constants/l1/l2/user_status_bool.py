023eb2d5a1340268479bdaa010db413cb46426a2
Eugen <eugen.hoppe.de@gmail.com>
2023-12-13 20:18:03 +0100

    system like ip address

--- v3/d5/constants/l1/l2/user_status_bool.py
+++ v3/d5/constants/l1/l2/user_status_bool.py
@@ -4,3 +4,7 @@ from d5.constants import Constant
 class UserStatusBool(Constant):  # parent 1.1.456
     ID_1 = (True, "active")
     ID_2 = (False, "not active")
+    # .       |         |
+    # Address |         |
+    #         V         V
+    # .  1.1.456.2  1.1.456.2.1
--- v3/d5_example.py
+++ v3/d5_example.py
@@ -4,22 +4,22 @@ from d5.dictionary import App, constants
 if __name__ == "__main__":
     # Example
 
-    # encoded data from db: 1.1.3 ->
+    # 1 - encoded data from db: 1.1.3 ->
     addr_1_1_3 = App.ID_1.value.ID_1.value.ID_3.value[0]
     print(addr_1_1_3)
 
-    # encoded data from db: 1.1.3.1 ->
+    # 2 - encoded data from db: 1.1.3.1 ->
     addr_1_1_3_1 = App.ID_1.value.ID_1.value.ID_3.value[1]
     print(addr_1_1_3_1)
 
-    # encoded data from db: 2.1.2.1 ->
+    # 3 - encoded data from db: 2.1.2.1 ->
     print(App.fetch([2, 1, 2, 1]))
 
-    # encoded data from db: 1.1.42.1 ->
+    # 4 - encoded data from db: 1.1.42.1 ->
     print(App.fetch("1.1.42.1"))
 
-    # encoded data from db: (1.)1.42.1 ->
-    print(constants.User.fetch("1.42.1"))
+    # 5 - encoded data from db: (1.)1.42.1 ->
+    print(constants.User.fetch("1.42.1"))  # same constant like 4
 
-    # encoded data
+    # 6 - encoded data
     print(App.fetch("1.1.456.2.1"))
