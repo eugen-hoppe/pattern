c5a979ccf8d8ba02924eb4bad7580344a9a8bdc5
Eugen <eugen.hoppe.de@gmail.com>
2023-12-13 18:38:00 +0100

    cc

--- v3/d5/constants/_const.py
+++ v3/d5/constants/_const.py
@@ -3,28 +3,18 @@ import re
 from enum import Enum
 
 
+# Configuration
+# =============
 PATH_TO_INIT_FILE = ["v3", "d5", "constants"]
 CONSTANT_NAMES = (
     "account",
     "user",
 )
+# TODO: for sub constants
 
 
-class To:
-    @staticmethod
-    def camel(snake: str, upper_first: bool = False) -> str:
-        if snake.startswith("_"):
-            snake = snake[1:]
-        parts = snake.lower().split("_")
-        if upper_first:
-            return "".join(p.capitalize() for p in parts)
-        return parts[0] + "".join(p.capitalize() for p in parts[1:])
-
-    @staticmethod
-    def snake(camel: str) -> str:
-        return re.sub(r"([a-z0-9])([A-Z])", r"\1_\2", camel).lower()
-
-
+# Main module
+# ===========
 class Constant(Enum):
     """
     Constant Instance
@@ -61,6 +51,26 @@ class Constant(Enum):
         
         with open(file=os.path.join(*path, f"__init__.py"), mode="w") as file_py:
             file_py.write(snippet)
+
+
+# Dependency Pattern
+# ==================
+class To:
+    """
+    Casepat: https://github.com/eugen-hoppe/pattern/tree/main/v3/casepat
+    """
+    @staticmethod
+    def camel(snake: str, upper_first: bool = False) -> str:
+        if snake.startswith("_"):
+            snake = snake[1:]
+        parts = snake.lower().split("_")
+        if upper_first:
+            return "".join(p.capitalize() for p in parts)
+        return parts[0] + "".join(p.capitalize() for p in parts[1:])
+
+    @staticmethod
+    def snake(camel: str) -> str:
+        return re.sub(r"([a-z0-9])([A-Z])", r"\1_\2", camel).lower()
 
 
 if __name__ == "__main__":
